bf5dddd0589682af5078973ba892723c3092ec0b
jafidiesel <jafibravin@outlook.com>
2024-04-29 18:33:19 -0300

    Initial api setup

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+const logger = (req, res, next) => {
+    console.log(res.statusCode, req.method, req.url);
+    next();
+}
+
+app.use(logger);
+
+
+// Define your routes here
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
