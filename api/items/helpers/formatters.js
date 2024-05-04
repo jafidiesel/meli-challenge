e5c7fec40297f32b0321fc88257bc0fb855c83d4
jafidiesel <jafibravin@outlook.com>
2024-05-04 20:20:26 -0300

    Added /items/:id endpoint. Improved code

--- api/items/helpers/formatters.js
+++ api/items/helpers/formatters.js
@@ -18,7 +18,26 @@ const formatCategories = (filters) => {
   if (!filters || filters.length === 0) return [];
   return [filters[0].values[0].path_from_root.map((category) => category.name)];
 };
+
+const formatItem = (item, itemDescription) => {
+  return {
+    id: item.id,
+    title: item.title,
+    price: {
+      currency: item.currency_id,
+      amount: item.price,
+      decimals: 2,
+    },
+    picture: item.thumbnail,
+    condition: item.condition,
+    free_shipping: item.shipping.free_shipping,
+    sold_quantity: item.sold_quantity,
+    description: itemDescription.plain_text,
+  };
+}
+
 module.exports = {
   formatItems,
   formatCategories,
+  formatItem
 };
--- api/items/service/items.service.js
+++ api/items/service/items.service.js
@@ -1,10 +1,9 @@
-const { formatItems, formatCategories} = require("../helpers/formatters");
-const getItemsRepository = require("../repository/getItems.repository");
+const { formatItems, formatCategories, formatItem} = require("../helpers/formatters");
+const { getItems, getItem, getItemDescription } = require("../repository/items.repository");
 
 const getAllItems = async (req, res) => {
   try {
-    const response = await getItemsRepository(req.query.q, req.query.limit, req.query.offset);
-    console.log(response)
+    const response = await getItems(req.query.q, req.query.limit, req.query.offset);
     res.status(200).send({
       author: {
         name: "",
@@ -14,16 +13,25 @@ const getAllItems = async (req, res) => {
       items: formatItems(response.results),
     });
   } catch (error) {
-    // this will be removed once the route validators are implemented
-    res.status(400).send({ message: error.message});
+    // this will be improved once the route validators are implemented
+    res.status(error.status ? error.status : 400).send({ message: error.message});
   }
 };
 
 const getItemById = async (req, res) => {
   try {
-    res.status(200).send({ id: req.params.id });
+    const itemResponse = await getItem(req.params.id);
+    const itemDescriptionResponse = await getItemDescription(req.params.id);
+    res.status(200).send({
+      author: {
+        name: "",
+        lastname: "",
+      },
+      item: formatItem(itemResponse, itemDescriptionResponse),
+    });
   } catch (error) {
-    res.status;
+    // this will be improved once the route validators are implemented
+    res.status(error.status ? error.status : 400).send({ message: error.message });
   }
 };
 
