20b075fae6d689b141577091c26fe8f700f99bbf
jafidiesel <jafibravin@outlook.com>
2024-05-10 21:17:24 -0300

    Added api headers

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 require('dotenv').config();
+const helmet = require('helmet');
+const cors = require('cors')
+
 const itemsController = require('./items/controller/items.controller');
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(helmet());
+app.use(cors());
 
 app.use( '/api/items', itemsController);
 
